0d67b6ffd1c4381cc7499834eb9a78df47afa4d1
José Rodrigues <josemiguel443@gmail.com>
2022-12-06 12:03:20 +0000

    Added add test

--- tests/store_test.go
+++ tests/store_test.go
@@ -7,8 +7,7 @@ import (
 	"github.com/JoseRodrigues443/is-my-team-awake/store"
 )
 
-func TestStore(t *testing.T) {
-
+func TestGetMemberStore(t *testing.T) {
 	repo := store.NewRepo()
 	got, _ := repo.GetMemberByName("Janet Doe")
 
@@ -19,5 +18,40 @@ func TestStore(t *testing.T) {
 
 	if got == nil {
 		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestAddMemberStore(t *testing.T) {
+	repo := store.NewRepo()
+	to_add := lib.TeamMember{
+		Name:     "Test 1",
+		Location: "Asia/Kolkata",
+	}
+	total_before := len(repo.GetAll())
+
+	repo.AddMember(to_add)
+
+	total_after := len(repo.GetAll())
+
+	if total_before == total_after {
+		t.Errorf("got %d, wanted %d", total_after, total_before+1)
+	}
+}
+
+func TestAddMemberRepeatedStore(t *testing.T) {
+	repo := store.NewRepo()
+	all := repo.GetAll()
+	to_add := lib.TeamMember{
+		Name:     all[0].Name,
+		Location: "Asia/Kolkata",
+	}
+	total_before := len(all)
+
+	repo.AddMember(to_add)
+
+	total_after := len(repo.GetAll())
+
+	if total_before < total_after {
+		t.Errorf("got %d, wanted %d", total_after, total_before)
 	}
 }
